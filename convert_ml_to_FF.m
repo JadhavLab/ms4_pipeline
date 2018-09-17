4728e5e5f078573d2caee7c56c1bad52a246d7fa
Roshan Nanu <rnanu@brandeis.edu>
2018-09-17 15:24:39 -0400

    Fix convert_ml_to_FF. Working!

--- convert_ml_to_FF.m
+++ convert_ml_to_FF.m
@@ -2,69 +2,76 @@ function out = convert_ml_to_FF(animID,resDir,sessionNum,varargin)
 
     samplerate = 30000;
     min_epoch_break=1;
+    overwrite = 0;
 
     assignVars(varargin)
 
+    out = '';
     tetDirs = dir([resDir filesep '*.mountain']);
-    timeFile = dir([resDir filesep '*timestamps*]);
+    timeFile = dir([resDir filesep '*timestamps*']);
     if isempty(timeFile)
         error('No timestamps file found in %s',resDir);
     end
     timeFile = [timeFile.folder filesep timeFile.name];
     timeDat = readmda(timeFile);
     saveFile = sprintf('%s%s%sspikes%02i.mat',resDir,filesep,animID,sessionNum);
+    if exist(saveFile,'file') && ~overwrite
+        fprintf('Spikes file @ %s already exists. Skipping...\n',saveFile)
+        return;
+    end
 
     % Determine epoch start times by looking for gaps longer than 1 sec
     gaps = diff(timeDat);
     epoch_gaps = find(gaps>=min_epoch_break*samplerate);
     epoch_starts = timeDat([1 epoch_gaps]);
     Nepochs = numel(epoch_starts);
+    fprintf('Detected %i epochs. Start times:\n',Nepochs);
+    disp(epoch_starts');
 
     % get tet numbers
     pat = '\w*.nt(?<tet>[0-9]+).\w*';
-    parsed = cellfun(@(x) regexp(x,pat,'names'),tetDirs);
-    tet_nums = [parsed.tet];
+    parsed = cellfun(@(x) regexp(x,pat,'names'),{tetDirs.name});
+    tet_nums = str2double({parsed.tet});
     Ntets = max(tet_nums);
+    fprintf('Detected %i tetrodes. Tetrodes:\n',Ntets);
+    disp(tet_nums');
+    drawnow;
 
     spikes = cell(1,sessionNum);
     spikes{sessionNum} = cell(1,Nepochs);
     spikes{sessionNum}(:) = {cell(1,Ntets)};
 
     for k=1:numel(tetDirs)
-        tD = tetDirs{k};
-        % get tet num
-        pat = '\w*.nt(?<tet>[0-9]+).\w*';
-        parsed = regexp(tD,pat,'names');
-        tetNum = str2double(parsed.tet);
+        tD = [tetDirs(k).folder filesep tetDirs(k).name];
+        tetNum = tet_nums(k);
         metFile = [tD filesep 'metrics_raw.json'];
-        fireFile = [tD filesep 'firings_raw.mda];
+        fireFile = [tD filesep 'firings_raw.mda'];
         metDat = jsondecode(fileread(metFile));
         metDat = metDat.clusters;
         fireDat = readmda(fireFile); % Rows are # Channels, timestamp (starting @ 0), cluster #
 
         clusters = [metDat.label];
         Nclust = max(clusters)+any(clusters==0);
-        spikes{sessionNum}(:) = {cell(1,Nclust)};
         [clusters,ic] = sort(clusters,'ascend');
         metDat = metDat(ic);
 
         % TODO: check params.json for smaplerate and error if not matching
 
         fireTimes = timeDat(fireDat(2,:));
-        tmp = cell(1,Nclust);
         for l=1:Nepochs
+            spikes{sessionNum}{l}{tetNum} = cell(1,Nclust);
             for m=1:Nclust
                 t1 = epoch_starts(l);
                 if l<Nepochs
-                    t2 = epoch_starts(l+1);
+                    t2 = epoch_starts(l+1)-1;
                 else
                     t2=timeDat(end);
                 end
-                idx = fireDat(3,:)==clusters(m) & fireTimes>t1 & fireTimes<t2;
+                idx = fireDat(3,:)==clusters(m) & fireTimes>=t1 & fireTimes<=t2;
                 mD = metDat(m).metrics;
                 dat = zeros(sum(idx),7);
-                fields = 'time x y dir not_used amplitude(highest variance channel) posindex detection_channels';
-                descript = 'spike data';
+                fields = 'time x y dir not_used amplitude(highest variance channel) posindex n_detection_channels';
+                descript = 'spike data from MountainSort 4 (MountainLab-JS)';
                 meanrate = mD.firing_rate;
                 peak_amplitude = mD.peak_amplitude;
                 refractory_violation_1msec = mD.refractory_violation_1msec;
@@ -82,5 +89,4 @@ function out = convert_ml_to_FF(animID,resDir,sessionNum,varargin)
     end
 
     save(saveFile,'spikes');   
-
-
+    out = saveFile;
