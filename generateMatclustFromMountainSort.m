b0e00b936349ab714c6e9f66ec9eb61647758d63
Roshan Nanu <rnanu@brandeis.edu>
2018-11-15 16:08:50 -0500

    GUI for visualizing mountainsort clusters with matclust params

--- generateMatclustFromMountainSort.m
+++ generateMatclustFromMountainSort.m
@@ -20,6 +20,10 @@ function out = generateMatclustFromMountainSort(tetResDir,varargin)
     rawFile = prvDat.original_path;
     timeFile = dir([fileparts(tetResDir) filesep  '*.timestamps*']);
     timeFile = [timeFile.folder filesep timeFile.name];
+    if strcmpi(timeFile(end-3:end),'.prv')
+        tmp = jsondecode(fileread(timeFile));
+        timeFile = tmp.original_path;
+    end
     %paramFile = [tetResDir filesep 'params.json'];
     %metFile = [tetResDir filesep 'metrics_raw.json'];
     destFolder = strrep(fileparts(tetResDir),'.mountain','.matclust');
@@ -35,7 +39,10 @@ function out = generateMatclustFromMountainSort(tetResDir,varargin)
 
     fprintf('Loading mountainsort data...\n')
     % Get and load trodes extracted spikes binary 
+    rawFile = strrep(rawFile,'/media/roshan/ExtraDrive1','/data');
     spikeDir = strrep(fileparts(rawFile),'.mda','.spikes');
+    spikeDir = strrep(spikeDir,'/media/roshan/ExtraDrive1','/data');
+    timeFile = strrep(timeFile,'/media/roshan/ExtraDrive1','/data');
     [~,datPrefix] = fileparts(spikeDir);
     spikeFile = [spikeDir filesep datPrefix '.spikes_nt' num2str(tet) '.dat'];
     if ~exist(spikeFile,'file')
@@ -63,7 +70,7 @@ function out = generateMatclustFromMountainSort(tetResDir,varargin)
 
     % create fields
     fprintf('Gathering spike waveforms...\n')
-    fireIdx = fireDat(2,:)-1; % -1 since mountainsort is in python and indexes from 0 where matlab indexes from 1
+    fireIdx = fireDat(2,:);
     fireTimes = timeDat(fireIdx);
     if isrow(fireTimes)
         fireTimes = fireTimes';
@@ -101,7 +108,6 @@ function out = generateMatclustFromMountainSort(tetResDir,varargin)
     %  createMatclustFile to generate matclust param and waves files
 %--------------------------------------------------------------------------------
 %--------------------------------------------------------------------------------
-
     fprintf('Generating matclust param and waves files...\n')
     maxSamplesToPeak = 17;
     minSamplesToPeak = 10;
@@ -179,6 +185,13 @@ function out = generateMatclustFromMountainSort(tetResDir,varargin)
         end
         filedata.params = [filedata.params scores];
 
+        if isrow(spikeClusters)
+            spikeClusters = spikeClusters';
+        end
+        filedata.params = [filedata.params spikeClusters];
+        filedata.paramnames = [filedata.paramnames,'Cluster'];
+
+
         waves = permute(spikeWaveforms,[2 3 1]);
     else
         disp('Not enough spike events in file');
@@ -200,90 +213,90 @@ function out = generateMatclustFromMountainSort(tetResDir,varargin)
         error('No param data')
     end
 
-    %--------------------------------------------------------------------------------
-    %--------------------------------------------------------------------------------
-
-    %% Now hack-and-slash a matclust save file in order to import the moutainsort clustering to matclust
-
-    %--------------------------------------------------------------------------------
-    %--------------------------------------------------------------------------------
-
-    fprintf('Now creating a matclust save file containing the mountainsort cluster...\n')
-    min_epoch_gap = 1;
-    samplerate = data.clockrate;
-
-    % Load matclust save file template
-    mcTemplate = load('matclust_file_template.mat');
-    clustdata = mcTemplate.clustdata;
-    clustattrib = mcTemplate.clustattrib;
-    graphattrib = mcTemplate.graphattrib;
-
-    %% Customize clustdata
-    nParams = size(filedata.params,2);
-    clustdata.filledparam = ones(1,nParams);
-    clustdata.params = filedata.params;
-    clustdata.origparams = nParams;
-    clustdata.names = filedata.paramnames;
-    nSpikes = size(filedata.params,1);
-    spikeTimes = filedata.params(:,1);
-
-    % create time filters
-    if isrow(timeDat)
-        timeDat = timeDat';
-    end
-    epoch_gaps = find(diff(timeDat)>=min_epoch_gap*samplerate);
-    epoch_start = double(timeDat([1 epoch_gaps+1]))./samplerate;
-    epoch_end = double(timeDat([epoch_gaps numel(timeDat)]))./samplerate;
-    timefilterranges = [epoch_start epoch_end];
-    timefilterranges = [double(timeDat([1 end]))'./samplerate+[-550 550];timefilterranges];
-    clustdata.timefilterranges = timefilterranges;
-
-    timefilters = int32(ones(nSpikes,1));
-    clustdata.otherfilters = timefilters;
-    timefilternames = clustdata.timefilternames;
-    for k=2:size(timefilterranges,1)
-        tfr = timefilterranges(k,:);
-        spikes_in_range = spikeTimes>=tfr(1) & spikeTimes<=tfr(2);
-        timefilters(spikes_in_range) = k*2-1;
-        tSec = fix(rem(tfr,60));
-        tMin = fix(rem(tfr/60,60));
-        tHour = fix(tfr/60/60);
-        timefilternames{k} = sprintf('%i e%i %i:%02i:%02i-%i:%02i:%02i',k,k-1,tHour(1),tMin(1),tSec(1),tHour(2),tMin(2),tSec(2)); 
-    end
-    clustdata.timefilters = timefilters;
-
-    clustdata.timefiltermemmap = zeros(32,1);
-    clustdata.timefiltermemmap(1:size(timefilterranges,1)) = 1:size(timefilterranges,1);
-    clustdata.filtermemmap = [clustdata.timefiltermemmap;clustdata.otherfiltermemmap];
-    clustdata.filteredpoints = true(nSpikes,1);
-    clustdata.dataranges = [min(filedata.params);max(filedata.params)];
-
-    %% Customize graphattrib
-    graphattrib.nonhiddenpoints = true(nSpikes,1);
-    graphattrib.currentdatarange = clustdata.dataranges(:,1:2);
-
-    %% Customize clustattrib
-    nClust = numel(unique(spikeClusters));
-    clustNums = sort(unique(spikeClusters),'ascend');
-    clusters = cell(1,nClust);
-    for k=1:nClust
-        clusters{k}.defineaxes=[];
-        clusters{k}.box={};
-        clusters{k}.polyindex=[];
-        clusters{k}.index=find(spikeClusters==clustNums(k));
-    end
-    clustattrib.clusters = clusters;
-    clustattrib.filterindex = {[1 1]};
-    clustattrib.clustersOn = ones(nClust,1);
-    clustattrib.currentfilepath = destFolder;
-    clustattrib.currentfilename = ['mountain_param_nt' num2str(data.ntrode_id) '_template.mat'];
-    clustattrib.currentparamfilename = ['param_nt' num2str(data.ntrode_id) '.mat'];
-    clustattrib.dataFile = ['waves_nt' num2str(data.ntrode_id) '.mat'];
-    clustattrib.pointexclude = int32(zeros(nSpikes,1));
-    clustattrib.pointinclude = int32(zeros(nSpikes,1));
-    clustattrib.currclust = 1;
-
-    save([destFolder filesep clustattrib.currentfilename],'clustdata','graphattrib','clustattrib')
+%    %--------------------------------------------------------------------------------
+%    %--------------------------------------------------------------------------------
+%
+%    %% Now hack-and-slash a matclust save file in order to import the moutainsort clustering to matclust
+%
+%    %--------------------------------------------------------------------------------
+%    %--------------------------------------------------------------------------------
+%
+%    fprintf('Now creating a matclust save file containing the mountainsort cluster...\n')
+%    min_epoch_gap = 1;
+%    samplerate = data.clockrate;
+%
+%    % Load matclust save file template
+%    mcTemplate = load('matclust_file_template.mat');
+%    clustdata = mcTemplate.clustdata;
+%    clustattrib = mcTemplate.clustattrib;
+%    graphattrib = mcTemplate.graphattrib;
+%
+%    %% Customize clustdata
+%    nParams = size(filedata.params,2);
+%    clustdata.filledparam = ones(1,nParams);
+%    clustdata.params = filedata.params;
+%    clustdata.origparams = nParams;
+%    clustdata.names = filedata.paramnames;
+%    nSpikes = size(filedata.params,1);
+%    spikeTimes = filedata.params(:,1);
+%
+%    % create time filters
+%    if isrow(timeDat)
+%        timeDat = timeDat';
+%    end
+%    epoch_gaps = find(diff(timeDat)>=min_epoch_gap*samplerate);
+%    epoch_start = double(timeDat([1 epoch_gaps+1]))./samplerate;
+%    epoch_end = double(timeDat([epoch_gaps numel(timeDat)]))./samplerate;
+%    timefilterranges = [epoch_start epoch_end];
+%    timefilterranges = [double(timeDat([1 end]))'./samplerate+[-550 550];timefilterranges];
+%    clustdata.timefilterranges = timefilterranges;
+%
+%    timefilters = int32(ones(nSpikes,1));
+%    clustdata.otherfilters = timefilters;
+%    timefilternames = clustdata.timefilternames;
+%    for k=2:size(timefilterranges,1)
+%        tfr = timefilterranges(k,:);
+%        spikes_in_range = spikeTimes>=tfr(1) & spikeTimes<=tfr(2);
+%        timefilters(spikes_in_range) = k*2-1;
+%        tSec = fix(rem(tfr,60));
+%        tMin = fix(rem(tfr/60,60));
+%        tHour = fix(tfr/60/60);
+%        timefilternames{k} = sprintf('%i e%i %i:%02i:%02i-%i:%02i:%02i',k,k-1,tHour(1),tMin(1),tSec(1),tHour(2),tMin(2),tSec(2)); 
+%    end
+%    clustdata.timefilters = timefilters;
+%
+%    clustdata.timefiltermemmap = zeros(32,1);
+%    clustdata.timefiltermemmap(1:size(timefilterranges,1)) = 1:size(timefilterranges,1);
+%    clustdata.filtermemmap = [clustdata.timefiltermemmap;clustdata.otherfiltermemmap];
+%    clustdata.filteredpoints = true(nSpikes,1);
+%    clustdata.dataranges = [min(filedata.params);max(filedata.params)];
+%
+%    %% Customize graphattrib
+%    graphattrib.nonhiddenpoints = true(nSpikes,1);
+%    graphattrib.currentdatarange = clustdata.dataranges(:,1:2);
+%
+%    %% Customize clustattrib
+%    nClust = numel(unique(spikeClusters));
+%    clustNums = sort(unique(spikeClusters),'ascend');
+%    clusters = cell(1,nClust);
+%    for k=1:nClust
+%        clusters{k}.defineaxes=[];
+%        clusters{k}.box={};
+%        clusters{k}.polyindex=[];
+%        clusters{k}.index=find(spikeClusters==clustNums(k));
+%    end
+%    clustattrib.clusters = clusters;
+%    clustattrib.filterindex = {[1 1]};
+%    clustattrib.clustersOn = ones(nClust,1);
+%    clustattrib.currentfilepath = destFolder;
+%    clustattrib.currentfilename = ['mountain_param_nt' num2str(data.ntrode_id) '_template.mat'];
+%    clustattrib.currentparamfilename = ['param_nt' num2str(data.ntrode_id) '.mat'];
+%    clustattrib.dataFile = ['waves_nt' num2str(data.ntrode_id) '.mat'];
+%    clustattrib.pointexclude = int32(zeros(nSpikes,1));
+%    clustattrib.pointinclude = int32(zeros(nSpikes,1));
+%    clustattrib.currclust = 1;
+%
+%    save([destFolder filesep clustattrib.currentfilename],'clustdata','graphattrib','clustattrib')
     fprintf('Done!\n')
 
 
